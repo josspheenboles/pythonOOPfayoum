de3d018855c3fcf6c1a9655ac125375b1a980a00
josspheenboles <eng.jospheenboles@gmail.com>
2024-08-10 13:01:33 +0300

    Day1

---
+++ Day1/demo.py
@@ -0,0 +1,129 @@
+x=10
+y=20
+print(x,y)
+x,y=y,x
+# z=x
+# x=y
+# y=z
+print(x,y)
+
+# name='aya ali'
+# statment=f"name: {name}"
+# print(statment)
+# s="trainee {id}\nName:{name} id:{id}"
+# print(s.format(name='aya' ,id=1))
+# print(s.format(1,'aya ali',1))
+# print(s.format(2,'ahmed ali',2))
+# print(s.format('ahmed ali',3,3))
+# fullname=input('enter fullname')
+# fullname='aya'
+# name=fullname.split(' ')
+# fullname.startwith()
+# s='aya aaaa ay '
+# news=s.replace('ay','@')
+# print(s,news)
+# username=input('enter your usernaem')
+# print(len(username),username.lower())
+# if(usernaem.isdigit() or usernaem.isalapa()):
+#     print('invalid username')
+# if(usernaem.isspace()):
+#     print('invalid usernaem')
+
+# # s='hello '
+
+# print(type(s))
+# print(s+' python track')
+# print(s*2)
+# #string collection of char--->each char accesess by index 
+# s='hello'
+# print(s,s[0],s[0:3])
+# #silsing collectionvar[[start=0],[end=len collection],[step=1])
+# print(s[::-1])#0,5,-1 -1,-2,-3,-4,-5
+# print(s[1:3])
+# x=False
+# print(x==-1)#compresion values only
+# print(max(1,2,3,45,67,89,23,-10))
+#declare multiple variable in  (single line)python 
+# varname1,varname2...varname3=val1,val2...val3
+#unpacking
+# varnam1,var....n=v1,....v2 collection tuple
+#count = count
+# x,y,*z=3,'one',3.3,9 #unpacking collectin into sperated variable
+# print(x,y,z)
+# # Data types
+# x= 3+2j #10.4
+# y=2+5j #20.5
+# print(x+y,x-y,x*2,x/2)
+# # int 
+# x=12
+# print(type(x))
+# # float 
+# y=13.4
+# print(type(y))
+# # complex
+# z=2+3j
+# print(type(z))
+# import sys
+
+# print(sys.maxsize)#different
+# print('=','' or 'python','=')#True 'bash' ......python
+# #check values same--->true and check data type the same False
+# print(3 =='3')#comparsion operation values 
+
+# print(3+5,4*3,16//5,16/5)
+# print(True and False)
+# print(1,'one',1.3,True,[],(),sep='\n',end='&')#spertator ' ' end /n
+# print('plapalp')
+
+# num=input('enter num')
+# if(num.isdigit()):
+#     num=int(num)
+# print(type(num))
+# x='18' #17.5
+# print(type(x))
+# x=int(x)
+# print(type(x))
+# x=0
+# x=10
+# t=(3,4,52,'dd')
+# _34x=10
+#str multi line not assign to var
+"""
+#single line comment 
+s="aya ali"#single line string
+print(s)
+s='aya \n ali'
+print(s)
+#multiline string ''' 
+s='''aya
+ali
+track python
+fayoum branch '''
+print(s)
+
+# x=2#block1
+# if x==3:#block1
+#     print('three')#block2
+#     if(True):#block2
+#         print('plplapa')#block3
+# elif x==4:#block1
+#     print('four')#block2
+# elif x==2:#block1
+#     print('two')#block2
+# else:#block1
+#     print('invalid number')#block2
+# #str,int,float,bool,complex,tuple,list,dict
+# x='one' #object of class str
+# print(x)
+# print(type(x))
+# x=10
+# print(x)
+# print(type(x))
+# x=10.5
+# print(x)
+# print(type(x))
+# x=True
+# print(x)
+# print(type(x))
+
+"""
